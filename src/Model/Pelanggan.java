5783bdf101f56a303160bc71a1c576e0ae6111e4
ArungRamadhan <65582917+ArungRamadhan@users.noreply.github.com>
2021-07-01 17:13:18 +0700

    Add files via upload

---
+++ src/Model/Pelanggan.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Model;
+
+/**
+ *
+ * @author user
+ */
+public class Pelanggan {
+    String alamat_p;
+    String id_p;
+    String nama_p;
+    String nohp_p;
+
+    public String getAlamat_p() {
+        return alamat_p;
+    }
+
+    public void setAlamat_p(String alamat_p) {
+        this.alamat_p = alamat_p;
+    }
+
+    public String getId_p() {
+        return id_p;
+    }
+
+    public void setId_p(String id_p) {
+        this.id_p = id_p;
+    }
+
+    public String getNama_p() {
+        return nama_p;
+    }
+
+    public void setNama_p(String nama_p) {
+        this.nama_p = nama_p;
+    }
+
+    public String getNohp_p() {
+        return nohp_p;
+    }
+
+    public void setNohp_p(String nohp_p) {
+        this.nohp_p = nohp_p;
+    }
+}
