446628a8516a9ce45bf05c2b98f6d13a3c2a38af
ArungRamadhan <65582917+ArungRamadhan@users.noreply.github.com>
2021-07-01 17:12:36 +0700

    Add files via upload

---
+++ src/Model/Airgalon.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Model;
+
+/**
+ *
+ * @author user
+ */
+public class Airgalon {
+    String id_air;
+    String keterangan_air;
+
+    public String getId_air() {
+        return id_air;
+    }
+
+    public void setId_air(String id_air) {
+        this.id_air = id_air;
+    }
+
+    public String getKeterangan_air() {
+        return keterangan_air;
+    }
+
+    public void setKeterangan_air(String keterangan_air) {
+        this.keterangan_air = keterangan_air;
+    }
+}
